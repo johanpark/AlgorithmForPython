499d753ac95c25354741a69eb3732e4ae1b6af34
Johan Park <54235431+johanpark@users.noreply.github.com>
2021-09-05 07:17:37 +0000

    BOJ_10816 commit

---
+++ BOJ/BOJ_10816.py
@@ -0,0 +1,16 @@
+import sys
+n = int(input())
+cardList = list()
+
+for _ in range(n):
+  cardList.append(int(input()))
+  
+m = int(input())
+answerList = list()
+
+for _ in range(m)
+  target = int(input())
+  countNum = cardList.count(target)
+  answerList.append(countNum)
+
+print(answerList)
